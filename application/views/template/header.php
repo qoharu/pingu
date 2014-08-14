f788980372979e2da0baf882d19db2bf0c110de4
squhart <squhart@gmail.com>
2014-08-14 16:35:20 +0700

    Upload assets and full template

---
+++ application/views/template/header.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pinguin - Sebuah website laboratorium CNC</title>
+
+    <!-- CSS Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- buat favicon -->
+    <link rel="shortcut icon" href="img/favicon-pinguin.ico" >
+    <link rel="icon" type="/image/ico"  href="img/favicon-pinguin.ico" >
+    
+    <!-- end -->
+  </head>
+
+  <body>
+      
+    <!-- Hiasan warna-warna paling atas -->
+      
+    <div class="top-color">
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+      
+    <!-- Navigasi atas -->  
+        
+    <nav class="navbar navbar-default" role="navigation">
+    <div class="container-fluid">
+      
+    <!-- Brand and toggle get grouped for better mobile display -->
+      
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.html">Pinguin</a>
+    </div>
+
+    <!-- Membuat link-link berubah dari float-left menjadi vertikal teratur -->
+      
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="forum.html">Forum</a></li>
+        <li><a href="#">Belajar</a></li>
+        <li><a href="download.html">Download</a></li>
+        <li><a href="#">Kontak</a></li>
+        <li><form class="search-container" action="#" method="POST"><input type="search" class="form-control search" placeholder="Cari..." >
+            <input type="submit" class="btn btn-search" value="Cari">
+            </form>
+        </li>
+          </ul>
+    </div>  <!-- /.navbar-collapse -->
+  </div>  <!-- /.container-fluid -->
+</nav>
